a9bcbe8bf69403dbf7780843fe362cf8e1f02c95
Takeshi <a86487817@gmail.com>
2015-03-19 12:56:50 +0800

    update tree topo

--- mininet/tree/tree.py
+++ mininet/tree/tree.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 
 from mininet.cli import CLI
-from mininet.link import Link
+from mininet.node import Link
 from mininet.net import Mininet
 from mininet.node import RemoteController
 from mininet.term import makeTerm
+from functools import partial
+
 def ofp_version(switch, protocols):
   protocols_str = ','.join(protocols)
   command = 'ovs-vsctl set Bridge %s protocols=%s' % (switch, protocols)
   switch.cmd(command.split(' '))
 
 if '__main__' == __name__:
-  net = Mininet(controller=RemoteController)
+  net = Mininet(controller=partial( RemoteController, ip='10.42.0.27', port=6633 ))
   c0 = net.addController('c0')
   s1 = net.addSwitch('s1')
   s2 = net.addSwitch('s2')
@@ -25,16 +27,16 @@ if '__main__' == __name__:
   h3 = net.addHost('h3')
   h4 = net.addHost('h4')
 
-  Link(s1, h1)
-  Link(s2, h2)
-  Link(s5, h3)
-  Link(s6, h4)
+  net.addLink(s1, h1)
+  net.addLink(s2, h2)
+  net.addLink(s5, h3)
+  net.addLink(s6, h4)
 
-  Link(s1, s2)
-  Link(s2, s3)
-  Link(s2, s4)
-  Link(s4, s5)
-  Link(s4, s6)
+  net.addLink(s1, s2)
+  net.addLink(s2, s3)
+  net.addLink(s2, s4)
+  net.addLink(s4, s5)
+  net.addLink(s4, s6)
 
   net.build()
   c0.start()
