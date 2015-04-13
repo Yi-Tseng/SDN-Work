443fa498f1bff220ae65e906f7b743b0172cc59b
Yi Tseng <a86487817@gmail.com>
2015-04-13 20:03:05 +0800

    Update GetSwitches.py

--- GetSwitches/GetSwitches.py
+++ GetSwitches/GetSwitches.py
@@ -9,28 +9,9 @@ from ryu.ofproto import ofproto_v1_3
 
 
 class GetSwitches(app_manager.RyuApp):
-    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
-
     switches = []
     def __init__(self, *args, **kwargs):
         super(GetSwitches, self).__init__(*args, **kwargs)
-
-    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def packet_in_handler(self, ev):
-
-        msg = ev.msg
-        dp = msg.datapath
-        ofp = dp.ofproto
-        ofp_parser = dp.ofproto_parser
-        in_port = msg.match['in_port']
-        actions = [ofp_parser.OFPActionOutput(ofp.OFPP_FLOOD)]
-        out = ofp_parser.OFPPacketOut(
-            datapath=dp, 
-            buffer_id=msg.buffer_id, 
-            in_port=in_port,  
-            actions=actions)
-
-        dp.send_msg(out)
 
     @set_ev_cls(dpset.EventDP, MAIN_DISPATCHER)
     def datapath_change_handler(self, ev):
