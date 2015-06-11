0db5baa7ecce3d0a1f4cf2f53f37ecaffeb335a5
Takeshi <a86487817@gmail.com>
2015-06-11 17:13:32 +0800

    identify cross domain lldp packet

--- MultiControl/hierarchy/local.py
+++ MultiControl/hierarchy/local.py
@@ -5,6 +5,8 @@ from ryu.base import app_manager
 from ryu.ofproto import ofproto_v1_3
 from ryu.topology import event
 from ryu.topology import api
+from ryu.topology.switches import LLDPPacket
+from ryu.lib.packet import packet, ethernet, lldp
 from ryu.controller.handler import set_ev_cls
 from ryu.controller.handler import MAIN_DISPATCHER
 
@@ -18,3 +20,21 @@ class LocalControllerApp(app_manager.RyuApp):
         super(LoadBalanceApp, self).__init__(*args, **kwargs)
 
     
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+
+        msg = ev.msg
+        try:
+            src_dpid, src_port_no = LLDPPacket.lldp_parse(msg.data)
+
+            switch = api.get_switch(src_dpid)
+            if switch == None:
+                pass
+                # send cross domain link add
+            return
+        except LLDPPacket.LLDPUnknownFormat as e:
+            # This handler can receive all the packtes which can be
+            # not-LLDP packet. Ignore it silently
+            pass
+
+        # non-LLDP
