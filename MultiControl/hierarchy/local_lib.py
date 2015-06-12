93f30ad41cac373e6796301432831d30c0e0f016
Takeshi <a86487817@gmail.com>
2015-06-12 18:58:31 +0800

    define basic functions

---
+++ MultiControl/hierarchy/local_lib.py
@@ -0,0 +1,20 @@
+
+
+
+class LocalControllerLib(object):
+
+    def __init__(self):
+        pass
+
+
+    def send_cross_domain_link(self, local_dpid, local_port, out_dpid, out_port):
+        pass
+
+    def response_host(self):
+        pass
+
+    def response_dpid(self):
+        pass
+
+    def get_route(self):
+        pass
