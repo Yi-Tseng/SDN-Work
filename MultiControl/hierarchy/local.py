e3ef9aa9b67f79a2eacebc4b39a9b55477273289
Takeshi <a86487817@gmail.com>
2015-06-16 21:57:52 +0800

    add local routing

--- MultiControl/hierarchy/local.py
+++ MultiControl/hierarchy/local.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
-
+import networkx as nx
 from ryu.base import app_manager
 from ryu.ofproto import ofproto_v1_3
 from ryu.topology import event
@@ -24,7 +24,10 @@ class LocalControllerApp(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(LocalControllerApp, self).__init__(*args, **kwargs)
         self.local_lib = kwargs['local_lib']
+        self.local_lib.start_serve()
         self.global_port = {}
+        self.route_list = []
+        self.graph = None
     
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
@@ -36,10 +39,10 @@ class LocalControllerApp(app_manager.RyuApp):
             src_dpid, src_port_no = LLDPPacket.lldp_parse(msg.data)
             # dst: this switch
             dst_dpid, dst_port_no = datapath.dpid, msg.match['in_port']
-            switch = api.get_switch(src_dpid)[0]
+            switch = api.get_switch(src_dpid)
 
             # not this topology switch
-            if switch != None:
+            if len(switch) != 0:
                 return
 
             # send cross domain link add
@@ -50,12 +53,108 @@ class LocalControllerApp(app_manager.RyuApp):
             self.global_port[dst_dpid].append(dst_port_no)
 
             return
-        except LLDPPacket.LLDPUnknownFormat as e:
+        except LLDPPacket.LLDPUnknownFormat:
             # This handler can receive all the packtes which can be
             # not-LLDP packet. Ignore it silently
             pass
 
         # non-LLDP
+        # local routing
+        dpid = datapath.id
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        dst = eth.dst
+
+        if dst not in self.local_lib.hosts:
+            # can't find host in local topology
+            # ask global and let this msg queued
+            self.route_list.append((dst, msg))
+            self.local_lib.get_route(dst)
+            return
+
+        # found in local, do local routing
+        # two case:
+        # 1. In same switch
+        # 2. Not in same switch
+        host = self.local_lib.hosts[dst]
+
+        
+        if host[0] == dpid:
+            # same switch
+            out_port = host[1]
+            self._packet_out(msg, out_port)
+
+        else:
+            # not same switch
+            # calculate path
+            g = nx.Graph()
+            links = api.get_all_link(self)
+
+            for link in links:
+                src = int(link.src.dpid)
+                dst = int(link.dst.dpid)
+                g.add_edge(src, dst)
+            src = int(dpid)
+            dst = int(host[0])
+            path = None
+
+            if nx.has_path(g, src, dst):
+                path = nx.shortest_path(g, src, dst)
+
+            if path == None:
+                return
+            out_port = self._get_out_port(src, dst)
+            self._packet_out(msg, out_port)
+
+    def _get_out_port(self, src, dst):
+        '''
+        get output port from src switch to dst switch
+        return
+        '''
+        links = api.get_all_link(self)
+
+        for link in links:
+
+            if int(link.src.dpid) == src and \
+                int(link.dst.dpid) == dst:
+                return int(link.src.port_no)
+
+
+    def _flood_packet(self, msg):
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        data = None
+
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out_port = ofproto.OFPP_FLOOD
+        actions = [parser.OFPActionOutput(out_port)]
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions,
+                                  data=data)
+        datapath.send_msg(out)
+
+    def _packet_out(self, msg, out_port):
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        data = None
+
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        actions = [parser.OFPActionOutput(out_port)]
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions,
+                                  data=data)
+        datapath.send_msg(out)
 
     @set_ev_cls(local_lib.EventRouteResult, MAIN_DISPATCHER)
     def route_result_handler(self, ev):
@@ -63,11 +162,19 @@ class LocalControllerApp(app_manager.RyuApp):
 
     @set_ev_cls(local_lib.EventAskDpid, MAIN_DISPATCHER)
     def ask_dpid_handler(self, ev):
-        pass
+        dpid = ev.dpid
+        switch = api.get_switch(str(dpid))
+
+        if len(switch) != 0:
+            self.local_lib.response_dpid(dpid)
+
 
     @set_ev_cls(local_lib.EventAskHost, MAIN_DISPATCHER)
     def ask_host_handler(self, ev):
-        pass
+        host_mac = ev.host
+
+        if host_mac in self.local_lib.hosts:
+            self.local_lib.response_host(host_mac)
 
     @set_ev_cls(local_lib.EventHostDiscovery, MAIN_DISPATCHER)
     def host_discovery_handler(self, ev):
