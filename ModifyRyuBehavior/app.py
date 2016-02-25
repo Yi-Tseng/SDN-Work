2d0390659599bd70245c2011070997d26a06a0d5
Takeshi <a86487817@gmail.com>
2016-02-25 17:57:11 +0800

    Add ModifyRyuBehavior project

---
+++ ModifyRyuBehavior/app.py
@@ -0,0 +1,28 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+
+
+def warper(func):
+
+    def new_handler(dp, msg):
+        print("Msg : {}".format(msg))
+        func(dp, msg)
+
+    return new_handler
+
+
+class ChangeSendMsg(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(ChangeSendMsg, self).__init__(*args, **kwargs)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        datapath.send_msg = warper(datapath.send_msg)
