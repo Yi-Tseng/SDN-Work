f1ddbf5e57c38ec274dd811854ad0fd7e891e91e
Takeshi <a86487817@gmail.com>
2016-02-26 02:23:50 +0800

    Fix bugs for ModifyRyuBehavior

--- ModifyRyuBehavior/app.py
+++ ModifyRyuBehavior/app.py
@@ -2,22 +2,22 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_3
+from ryu.ofproto import ofproto_v1_0
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 
 
 def warper(func):
 
-    def new_handler(dp, msg):
+    def new_handler(msg):
         print("Msg : {}".format(msg))
-        func(dp, msg)
+        func(msg)
 
     return new_handler
 
 
 class ChangeSendMsg(app_manager.RyuApp):
-    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         super(ChangeSendMsg, self).__init__(*args, **kwargs)
@@ -26,3 +26,4 @@ class ChangeSendMsg(app_manager.RyuApp):
     def switch_features_handler(self, ev):
         datapath = ev.msg.datapath
         datapath.send_msg = warper(datapath.send_msg)
+
